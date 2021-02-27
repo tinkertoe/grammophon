c294ab2aedabc2e8803817fedf2cccc0c9576837
tinkertoe <40361609+tinkertoe@users.noreply.github.com>
2021-02-27 18:58:03 +0100

    maybe closing twice works?

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,6 @@ const device = new escpos.USB()
 const printer = new escpos.Printer(device)
 
 device.open((err) => {
-  // Print welcome message
   printer
     .font('B')
     .align('CT')
@@ -37,41 +36,38 @@ device.open((err) => {
     .feed(1)
     .cut()
     .close()
+}).close().close()
 
-  // Handle result of speech recognition
-  const handleResult = (result: Object) => {
-    // Check if result has text and that something was said
-    if (result.hasOwnProperty('text') && result['text'] != '') {
 
-      // Log result in console
-      log(chalk.yellow('> ' + result['text']))
 
+// Handle result of speech recognition
+const handleResult = (result: Object) => {
+  // Check if result has text and that something was said
+  if (result.hasOwnProperty('text') && result['text'] != '') {
+
+    // Log result in console
+    log(chalk.yellow('> ' + result['text']))
+
+    device.open((err) => {
       printer
         .align('LT')
         .style('NORMAL')
         .text('> ' + result['text'])
         .cut()
         .close()
-    }
+    }).close().close()
   }
+}
 
-  // When new data arrives on the microphone stream, send it to the recognition engine
-  micStream.on('data', (data) => {
-    // Function returns true if it thinks the sentence is over
-    const eof = rec.acceptWaveform(data)
-    if (eof) {
-      handleResult(JSON.parse(rec.result()))
-    }
-  })
-
-  // Start listening
-  mic.start()
-  log(chalk.green('Listening 🎤'))
-
+// When new data arrives on the microphone stream, send it to the recognition engine
+micStream.on('data', (data) => {
+  // Function returns true if it thinks the sentence is over
+  const eof = rec.acceptWaveform(data)
+  if (eof) {
+    handleResult(JSON.parse(rec.result()))
+  }
 })
 
-
-
-
-
-
+// Start listening
+mic.start()
+log(chalk.green('Listening 🎤'))
