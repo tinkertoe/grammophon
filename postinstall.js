218a4bcf21ed2327adc0dd2e2d5472dd6b76685c
tinkertoe <40361609+tinkertoe@users.noreply.github.com>
2021-02-28 14:37:28 +0100

    change spawn to exec

--- postinstall.js
+++ postinstall.js
@@ -1,6 +1,10 @@
 const spawnSync = require('child_process').spawnSync
 const os = require('os')
+const exec = require("child_process").exec
 if ((os.arch() == 'arm' || os.arch() == 'arm64' ) && process.platform != 'win32') {
   console.log('Replacing libvosk.so library')
-  spawnSync("cp libvosk.so node_modules/vosk/lib/linux-x86_64/libvosk.so")
+  exec("cp libvosk.so node_modules/vosk/lib/linux-x86_64/libvosk.so", (err, stdout, stderr) => {
+    if (stdout) { console.log(stdout) }
+    if (stderr) { console.error(stderr) }
+  }).unref()
 }
