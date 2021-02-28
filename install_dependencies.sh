75581b0375c1307b513cdda5df5e2323013d7971
tinkertoe <40361609+tinkertoe@users.noreply.github.com>
2021-02-28 16:26:44 +0100

    move installation of dependencies to bash file

---
+++ install_dependencies.sh
@@ -0,0 +1,7 @@
+sudo apt update -y
+sudo apt install -y curl
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update -y
+sudo apt install -y alsa-utils build-essential libudev-dev gcc g++ make python nodejs yarn
--- preinstall.js
+++
@@ -1,8 +0,0 @@
-const exec = require("child_process").exec
-if (process.platform == 'linux') {
-  console.log('Installing system dependencies')
-  exec("sudo apt update -y && sudo apt install -y alsa-utils build-essential libudev-dev python", (err, stdout, stderr) => {
-    if (stdout) { console.log(stdout) }
-    if (stderr) { console.error(stderr) }
-  }).unref()
-}
