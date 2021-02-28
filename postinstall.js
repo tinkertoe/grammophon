4d50df41cd03495548591671bb428881134680d7
tinkertoe <40361609+tinkertoe@users.noreply.github.com>
2021-02-28 15:41:30 +0100

    remove windows check

--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,7 @@
 const spawnSync = require('child_process').spawnSync
 const os = require('os')
 const exec = require("child_process").exec
-if ((os.arch() == 'arm' || os.arch() == 'arm64' ) && process.platform != 'win32') {
+if (os.arch() == 'arm' || os.arch() == 'arm64' ) {
   console.log('Replacing libvosk.so library')
   exec("cp libvosk.so node_modules/vosk/lib/linux-x86_64/libvosk.so", (err, stdout, stderr) => {
     if (stdout) { console.log(stdout) }
