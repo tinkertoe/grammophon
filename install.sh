e16454ad075f70d1f7fdb0e790e16d708d53f236
tinkertoe <40361609+tinkertoe@users.noreply.github.com>
2021-02-28 20:04:48 +0100

    lol

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update -y
 sudo apt-get install -y alsa-utils build-essential libudev-dev gcc g++ make python nodejs yarn
+yarn install
