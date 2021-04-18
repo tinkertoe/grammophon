94ccdd2d4c08052bf22c53a5986ed518f93fa28f
tinkertoe <40361609+tinkertoe@users.noreply.github.com>
2021-04-18 14:14:54 +0200

    clean up + check for linux

--- postinstall.js
+++ postinstall.js
@@ -1,10 +1,13 @@
-const spawnSync = require('child_process').spawnSync
 const os = require('os')
 const exec = require("child_process").exec
-if (os.arch() == 'arm' || os.arch() == 'arm64' ) {
+
+if (os.platform() === 'linux' && (os.arch() === 'arm' || os.arch() === 'arm64') ) {
+  
   console.log('Replacing libvosk.so library')
+  
   exec("cp libvosk.so node_modules/vosk/lib/linux-x86_64/libvosk.so", (err, stdout, stderr) => {
     if (stdout) { console.log(stdout) }
     if (stderr) { console.error(stderr) }
   }).unref()
+  
 }
